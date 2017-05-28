d8538a0db94bfb1b1eb66b8b33f1d3aea0d69e80
aib <aibok42@gmail.com>
2017-05-28 23:47:24 +0300

    Add get/set to Board

--- ttt/tictactoe.py
+++ ttt/tictactoe.py
@@ -5,8 +5,14 @@ class Board:
 	def __init__(self, size=3, players=['X', 'O']):
 		self.size = size
 		self.players = players
-		self.board = [[self.EMPTY for y in range(size)] for x in range(size)]
+		self.board = [self.EMPTY] * (size * size)
 		self.turn = 0
+
+	def get(self, x, y):
+		return self.board[x + y*self.size]
+
+	def set(self, x, y, p):
+		self.board[x + y*self.size] = p
 
 	def draw(self, pmap=None):
 		fmt = (" . | . | . \n"
@@ -24,20 +30,20 @@ class Board:
 		for p in self.players:
 			# Horizontal
 			for y in range(self.size):
-				if all(map(lambda m: m == p, [self.board[x][y] for x in range(self.size)])):
+				if all(map(lambda m: m == p, [self.get(x, y) for x in range(self.size)])):
 					return p
 
 			# Vertical
 			for x in range(self.size):
-				if all(map(lambda m: m == p, [self.board[x][y] for y in range(self.size)])):
+				if all(map(lambda m: m == p, [self.get(x, y) for y in range(self.size)])):
 					return p
 
 			# Diagonal \
-			if all(map(lambda m: m == p, [self.board[d][d] for d in range(self.size)])):
+			if all(map(lambda m: m == p, [self.get(d, d) for d in range(self.size)])):
 				return p
 
 			# Diagonal /
-			if all(map(lambda m: m == p, [self.board[(self.size-1)-d][d] for d in range(self.size)])):
+			if all(map(lambda m: m == p, [self.get((self.size-1)-d, d) for d in range(self.size)])):
 				return p
 
 		if len(list(filter(lambda m: m == self.EMPTY, self.as_list()))) == 0:
@@ -46,10 +52,10 @@ class Board:
 		return None
 
 	def play(self, x, y):
-		if self.board[x][y] != self.EMPTY:
+		if self.get(x, y) != self.EMPTY:
 			return False
 
-		self.board[x][y] = self.players[self.turn]
+		self.set(x, y, self.players[self.turn])
 		self.turn = (self.turn + 1) % len(self.players)
 		return True
 
@@ -57,7 +63,7 @@ class Board:
 		return self.players[self.turn]
 
 	def as_list(self):
-		return [self.board[x][y] for y in range(self.size) for x in range(self.size)]
+		return [self.get(x, y) for y in range(self.size) for x in range(self.size)]
 
 	def as_tuples(self):
-		return [(x, y, self.board[x][y]) for y in range(self.size) for x in range(self.size)]
+		return [(x, y, self.get(x, y)) for y in range(self.size) for x in range(self.size)]
